77633a3460c20bd56c2d326f2ae100336e2ac7b0
ahydrogenatom <ntaylor@unbc.ca>
2016-02-26 13:01:36 -0800

    Testing world generation

--- KBS_Rover_Project/src/kbs_rover_project/MainController.java
+++ KBS_Rover_Project/src/kbs_rover_project/MainController.java
@@ -16,6 +16,7 @@ public class MainController {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        worldTesting.createRandomWorld();
     }
     
 }
