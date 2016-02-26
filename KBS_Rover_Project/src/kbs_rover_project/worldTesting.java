bb679e75eacaf5fdd3917094022d4408b0a1d563
ahydrogenatom <ntaylor@unbc.ca>
2016-02-26 13:01:19 -0800

    Added a small test class to make sure that world generation is working

---
+++ KBS_Rover_Project/src/kbs_rover_project/worldTesting.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kbs_rover_project;
+
+/**
+ *
+ * @author Nick Taylor
+ */
+public class worldTesting {
+    
+    //creates a random world from the default random world generation method
+    public static void createRandomWorld()
+    {
+        WorldModel model = new WorldModel();
+        model.generateRandomWorld();
+        String testString = model.toString();
+        System.out.print(testString);
+    }
+    
+}
