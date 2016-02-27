6ee881a6ec1bafc25e474827a5148a2e13347978
jraine4 <jraine4@gmail.com>
2016-02-27 14:13:28 -0800

    finished chose move funcion

--- KBS_Rover_Project/src/kbs_rover_project/Robot.java
+++ KBS_Rover_Project/src/kbs_rover_project/Robot.java
@@ -14,17 +14,19 @@ public class Robot {
     private WorldTile goal;
     private WorldTile current;
     private WorldTile last;
-    
+    private InferenceEngine logicUnit;
     
     // constructers
     //given world
     public Robot (WorldModel t){
         terra=t;
+        logicUnit=new InferenceEngine();
     }
     //given world and inital goal
     public Robot (WorldModel t,WorldTile g){
         terra=t;
         goal=g;
+        logicUnit=new InferenceEngine();
     }
     
     
@@ -57,19 +59,63 @@ public class Robot {
     
     //pick witch way to go
     public void chooseMove(){
-        int sA,sB,sC;
-        
+        int[] scores = new int[4];
+        WorldTile[] options = new WorldTile[4];
         int curx,cury;
         curx=current.getXCoord();
         cury=current.getYCoord();
+        int count=0;
         
         if(terra.getTile(curx+1,cury)!=last){
-            sA=Inferince
+                scores[count] = logicUnit.getNextAction(terra.getTile(curx+1,cury));
+                options[count]=terra.getTile(curx+1,cury);
+                count++;
+        }else{
+            scores[count]=-1;
+            options[count]=last;
+            count++;
         }
+        if(terra.getTile(curx,cury+1)!=last){
+                scores[count] = logicUnit.getNextAction(terra.getTile(curx,cury+1));
+                options[count]=terra.getTile(curx,cury+1);
+                count++;
+        }else{
+            scores[count]=-1;
+            options[count]=last;
+            count++;
+        }
+        if(terra.getTile(curx-1,cury)!=last){
+                scores[count] = logicUnit.getNextAction(terra.getTile(curx-1,cury));
+                options[count]=terra.getTile(curx-1,cury);
+                count++;
+        }else{
+            scores[count]=-1;
+            options[count]=last;
+            count++;
+        }
+        if(terra.getTile(curx,cury-1)!=last){
+                scores[count] = logicUnit.getNextAction(terra.getTile(curx,cury-1));
+                options[count]=terra.getTile(curx,cury-1);
+                count++;
+        }else{
+            scores[count]=-1;
+            options[count]=last;
+            count++;
+        }
+        int max=0;
+        for(int i=0;i<4;i++){
+            if(scores[i]>max){
+                max=i;
+            }  
+        }
+        move(options[max]);   
+    }
+    private void move(WorldTile place){
+        last=current;
+        current=place;
         
         
     }
-    
     
     
     
